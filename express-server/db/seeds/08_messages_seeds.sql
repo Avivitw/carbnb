6871b46e5a6ed344f78bb54ef3450647747a9bd7
Avivit Weissman <wavivit@gmail.com>
2021-07-22 13:31:18 -0700

    created messages seeds

---
+++ express-server/db/seeds/08_messages_seeds.sql
@@ -0,0 +1,10 @@
+INSERT INTO favourites(sender_id, receiver_id, message, created_at)
+VALUES
+  (3, 4,"Hello"),
+  (3, 4, "Hi"),
+  (2, 3, "What's up?"),
+  (3,2, "Everything is Okay"),
+  (4, 3, "How do you do?"),
+  (4, 2, "I'm fine"),
+  (2, 3, "Hi I wonder about your car"),
+  (3,4, "Do you have any questions?");
