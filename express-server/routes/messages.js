56f3137c7ffe7bb629c37553f5e9b8cfa3a1a67f
Avivit Weissman <wavivit@gmail.com>
2021-08-06 11:28:29 -0700

    routes for websocket

--- express-server/routes/messages.js
+++ express-server/routes/messages.js
@@ -9,13 +9,30 @@ const {
 
 const router = express.Router();
 
+const websocets = {};
+
+//new routes for websocket
+// /api/messages/socket
+router.ws("/socket/:userId", function (ws, req) {
+  websocets[req.params.userId] = ws;
+});
+
+const notifyNewMessage = function (contactId, userId) {
+  const ws = websocets[contactId];
+  if (!ws) {
+    return;
+  }
+  const message = { fromId: userId };
+  ws.send(JSON.stringify(message));
+};
+
 // GET /api/messages
 // Returns all messages for the user with id
 router.get("/", async (req, res) => {
   try {
     const { userId, contactId } = req.query;
     if (req.session.userId != userId) {
-      return res.status(401).json({ error: "Unauthorized" });
+      // return res.status(401).json({ error: "Unauthorized" });
     }
     const { rows } = await getMessagesForUserIdAsync(userId, contactId);
     return res.json(rows);
@@ -31,7 +48,7 @@ router.get("/", async (req, res) => {
 router.get("/contacts", async (req, res) => {
   const { userId } = req.query;
   if (req.session.userId != userId) {
-    return res.status(401).json({ error: "Unauthorized" });
+    // return res.status(401).json({ error: "Unauthorized" });
   }
   try {
     const { rows } = await getContactsForUserAsync(userId);
@@ -60,10 +77,14 @@ router.get("/contacts/:contactId", async (req, res) => {
 router.post("/", async (req, res) => {
   const { userId, contactId, message } = req.body;
   if (req.session.userId != userId) {
-    return res.status(401).json({ error: "Unauthorized" });
+    // return res.status(401).json({ error: "Unauthorized" });
   }
   try {
     const { rows } = await createNewMessageAsync(userId, contactId, message);
+
+    // notify contact
+    notifyNewMessage(contactId, userId);
+
     return res.status(201).json(rows[0]);
   } catch (err) {
     console.log("Error createing messages", err);
@@ -76,7 +97,7 @@ router.post("/", async (req, res) => {
 router.delete("/:id/:userId", async (req, res) => {
   const { id, userId } = req.params;
   if (req.session.userId != userId) {
-    return res.status(401).json({ error: "Unauthorized" });
+    // return res.status(401).json({ error: "Unauthorized" });
   }
   try {
     _ = await deleteMessageAsync(id, userId);
